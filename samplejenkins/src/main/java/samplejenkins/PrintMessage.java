4719e145d9fea354ca2525d1cc463d5d21a0b73b
kanna <kanna@192.168.1.39>
2025-04-17 16:54:45 +0530

    First commit

---
+++ samplejenkins/src/main/java/samplejenkins/PrintMessage.java
@@ -0,0 +1,10 @@
+package samplejenkins;
+
+public class PrintMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("from print class");
+	}
+
+}
